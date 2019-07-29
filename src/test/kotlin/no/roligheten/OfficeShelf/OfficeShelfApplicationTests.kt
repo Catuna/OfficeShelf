c994de7c32822ca8cdf5ecc342e18a00a681ded1
Christian Barth Roligheten <chrbarrol@gmail.com>
2019-07-29 22:58:47 +0200

    Inital commit (Spring initalizr project)

---
+++ src/test/kotlin/no/roligheten/OfficeShelf/OfficeShelfApplicationTests.kt
@@ -0,0 +1,16 @@
+package no.roligheten.OfficeShelf
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class OfficeShelfApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
